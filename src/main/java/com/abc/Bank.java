f8bab48366bff4abfa435b2dbee1e7ca23b8ff3f
infotechmadhu <infotechmadhu@yahoo.com>
2015-06-17 08:52:59 -0400

    Update Bank.java

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -1,5 +1,6 @@
 package com.abc;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,16 +10,28 @@ public class Bank {
     public Bank() {
         customers = new ArrayList<Customer>();
     }
-
+    
+    /**
+     * Add a customer object.
+     * @param customer
+     */
     public void addCustomer(Customer customer) {
         customers.add(customer);
     }
-
+    
+    /**
+     * Creates customer summary
+     * @return summary
+     */
     public String customerSummary() {
-        String summary = "Customer Summary";
-        for (Customer c : customers)
-            summary += "\n - " + c.getName() + " (" + format(c.getNumberOfAccounts(), "account") + ")";
-        return summary;
+    	   StringBuffer summary = new StringBuffer("Customer Summary");
+    	   for (Customer c : customers) {
+               summary.append("\n - ").append(c.getName());
+           	   summary.append(" (");
+               summary.append(format(c.getNumberOfAccounts(), "account"));
+               summary.append(")");
+           } 
+        return summary.toString();
     }
 
     //Make sure correct plural of word is created based on the number passed in:
@@ -26,14 +39,23 @@ public class Bank {
     private String format(int number, String word) {
         return number + " " + (number == 1 ? word : word + "s");
     }
-
-    public double totalInterestPaid() {
-        double total = 0;
-        for(Customer c: customers)
-            total += c.totalInterestEarned();
+    
+    /**
+     * Calculates the interest paid by customer.
+     * @return total. the total interest paid
+     */
+    public BigDecimal totalInterestPaid() {
+    	BigDecimal total = BigDecimal.ZERO;
+        for(Customer customer: customers)
+           // total += c.totalInterestEarned();
+        	total=total.add(customer.totalInterestEarned());
         return total;
     }
-
+    
+    /**
+     * Get first customer
+     * @return first customer name
+     */
     public String getFirstCustomer() {
         try {
             customers = null;
