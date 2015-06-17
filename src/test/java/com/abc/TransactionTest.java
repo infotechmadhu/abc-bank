0a56d29f644b7bb1be97aef2c02e22dd18a29979
infotechmadhu <infotechmadhu@yahoo.com>
2015-06-17 09:01:58 -0400

    Update TransactionTest.java

--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -1,13 +1,26 @@
 package com.abc;
 
+import java.math.BigDecimal;
+
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class TransactionTest {
+	private static final double DOUBLE_DELTA = 1e-15;
+	
     @Test
     public void transaction() {
-        Transaction t = new Transaction(5);
+        Transaction t = new Transaction(new BigDecimal(5));
         assertTrue(t instanceof Transaction);
     }
+    
+    @Test
+    public void testTransactionAmount(){
+    	Transaction t = new Transaction(new BigDecimal(5));
+    	assertEquals(5.0, t.amount.doubleValue(), DOUBLE_DELTA);
+    }
+
+	
 }
